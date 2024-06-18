662ba9a6aa79d47d29f98c43ed38e806375ad0cb
Ace10079 <minions10karan@gmail.com>
2024-06-18 09:44:47 +0530

    otp

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,19 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { auth } from "../firebase";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
 function SignUp() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [otp, setOtp] = useState("");
+  const [confirmationResult, setConfirmationResult] = useState(null);
   const navigate = useNavigate();
-  const handledashboard = ()=>{
-navigate('/dashboard');
-console.log('button clicked');
-  }
+
+  const setUpRecaptcha = () => {
+    if (!window.recaptchaVerifier) {
+      window.recaptchaVerifier = new RecaptchaVerifier(auth,"recaptcha-container",
+        {
+          size: "invisible",
+          callback: (response) => {
+            // reCAPTCHA solved, allow signInWithPhoneNumber.
+            handleSendOtp();
+          },
+        },
+        auth
+      );
+    }
+  };
+
+  const handleSendOtp = () => {
+    setUpRecaptcha();
+    const appVerifier = window.recaptchaVerifier;
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        setConfirmationResult(confirmationResult);
+        console.log("OTP sent");
+      })
+      .catch((error) => {
+        console.error("Error during signInWithPhoneNumber", error);
+      });
+  };
+
+  const handleVerifyOtp = () => {
+    if (confirmationResult) {
+      confirmationResult
+        .confirm(otp)
+        .then((result) => {
+          console.log("User signed in successfully.");
+          navigate("/dashboard");
+        })
+        .catch((error) => {
+          console.error("Error verifying OTP", error);
+        });
+    }
+  };
+
   return (
     <div className="flex lg:justify-center">
       <div className="relative lg:w-[765px] lg:h-[100px] w-[520px]">
-        <div className="bg-green-700 lg:p-2 lg:w-[765px] relative lg:h-[100px] w-[500px] h-[100px] ">
+        <div className="bg-green-700 lg:p-2 lg:w-[765px] relative lg:h-[100px] w-[500px] h-[100px]">
           <p className="text-white lg:p-0 text-xl lg:ml-2 p-2">Welcome back</p>
-          <p className="lg:mt-2 mt-2 text-white text-lg lg:ml-2 ml-2">Sign in to Continue your Search</p>
+          <p className="lg:mt-2 mt-2 text-white text-lg lg:ml-2 ml-2">
+            Sign in to Continue your Search
+          </p>
         </div>
         <img
           src="./login.png"
@@ -22,17 +67,31 @@ console.log('button clicked');
         />
         <input
           type="text"
-          className="absolute lg:top-[250px] lg:left-[16px] lg:w-96 rounded-md focus:outline-none focus:border-blue-500 text-black border border-black lg:p-3 lg:ml-36 top-[250px] left-[130px] p-3"
+          className="absolute lg:top-[250px] lg:left-[16px] lg:w-96 w-1/2 rounded-md focus:outline-none focus:border-blue-500 text-black border border-black lg:p-3 lg:ml-36 top-[250px] left-[110px] p-3"
           placeholder="Enter Your Phone Number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
         />
+        <button
+          className="bg-green-800 lg:pl-20 lg:pr-20 lg:pt-2 lg:pb-2 absolute lg:top-[310px] lg:left-60 text-white border rounded-lg top-[310px] left-[170px] pl-10 pr-10 pt-1 pb-1 mt-3"
+          onClick={handleSendOtp}
+        >
+          Send OTP
+        </button>
         <input
           type="text"
-          className="absolute lg:top-[320px] lg:left-[16px] lg:w-96 rounded-md focus:outline-none focus:border-blue-500 text-black border border-black lg:p-3 lg:ml-36 top-[330px] left-[130px] p-3"
-          placeholder="Enter Otp"
+          className="absolute lg:top-[380px] lg:left-[16px] lg:w-96 w-1/2 rounded-md focus:outline-none focus:border-blue-500 text-black border border-black lg:p-3 lg:ml-36 top-[380px] left-[110px] p-3"
+          placeholder="Enter OTP"
+          value={otp}
+          onChange={(e) => setOtp(e.target.value)}
         />
-        <button className="bg-green-800 lg:pl-20 lg:pr-20 lg:pt-2 lg:pb-2 absolute lg:top-[400px] lg:left-64 text-white border rounded-lg top-[420px] left-[170px] pl-10 pr-10 pt-1 pb-1" onClick={handledashboard}>
-          Login
+        <button
+          className="bg-green-800 lg:pl-20 lg:pr-20 lg:pt-2 lg:pb-2 absolute lg:top-[450px] lg:left-60 text-white border rounded-lg top-[450px] left-[170px] pl-10 pr-10 pt-1 pb-1"
+          onClick={handleVerifyOtp}
+        >
+          Verify OTP
         </button>
+        <div id="recaptcha-container"></div>
       </div>
     </div>
   );
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,19 +1,21 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAzEI0CD3EF4L_dcNzzjq-sazWeM7hZsrg",
-    authDomain: "oryza-dashboard.firebaseapp.com",
-    projectId: "oryza-dashboard",
-    storageBucket: "oryza-dashboard.appspot.com",
-    messagingSenderId: "392478071009",
-    appId: "1:392478071009:web:a14299474f5b4eb728bfa0",
-    measurementId: "G-JG6RG267P4"
+  apiKey: "AIzaSyAAjpeFEOKTtAIJZ1fpRvfguwqNoXgdXPE",
+  authDomain: "oryza-7ae36.firebaseapp.com",
+  projectId: "oryza-7ae36",
+  storageBucket: "oryza-7ae36.appspot.com",
+  messagingSenderId: "579404902976",
+  appId: "1:579404902976:web:3f93e1f2ad8c8a8c7365a5",
+  measurementId: "G-7E0FSJBNL3"
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-
-export { firebaseApp, auth };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
